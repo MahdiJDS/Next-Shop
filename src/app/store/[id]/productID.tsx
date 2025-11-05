0fdeb5d8bfe621d7e2d17afaa5a39c74bc48213a
Mahdi Jahed <mahdijahed56@gmail.com>
2025-11-05 09:55:17 +0330

    Use produxtRedux component for managment quantity

--- src/app/store/[id]/productID.tsx
+++ src/app/store/[id]/productID.tsx
@@ -1,6 +1,6 @@
 import { jsonType } from "@/types";
 import { products } from "@/utils/GetAPI";
-import React from "react";
+import ProductRedux from '../Components/productRedux'
 
 interface IDtype {
   title: string;
@@ -14,16 +14,24 @@ export default function ProductID({ title }: IDtype) {
   return (
     <div className="items-center justify-center w-[60%] flex">
       {filter.map((item) => (
-         <div 
-         key={item.id}
-         className="flex flex-col items-center md:flex-row gap-3 border-2 rounded-lg p-3 border-slate-400 text-center bg-slate-900 hover:-translate-y-1 duration-300 cursor-pointer">
-            <img
-                src={item.img}
-                alt="product image"
-                className="mx-auto w-[300px] h-[200px] bg-cover rounded-lg"
-            />
-            {item.title}
-            {item.description}
+        <div
+          key={item.id}
+          className="flex flex-col items-center md:flex-row gap-3 border-2 rounded-lg p-3 border-slate-400 text-center bg-slate-900 hover:-translate-y-1 duration-300 cursor-pointer">
+          <img
+            src={item.img}
+            alt="product image"
+            className="mx-auto w-[300px] h-[200px] bg-cover rounded-lg"
+          />
+          {item.title}
+          {item.description}
+          <ProductRedux
+            items={{
+              id: item.id,
+              name: item.title, 
+              price: 0, 
+              quantity: 1, 
+            }}
+          />
         </div>
       ))}
     </div>
