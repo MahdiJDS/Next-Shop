da8f5074d9589772a81717d822051e65b8c29eb5
Mahdi Jahed <mahdijahed56@gmail.com>
2025-11-11 17:14:29 +0330

    feat: update manegment (marge Local and Json)

--- src/context/productManagment.tsx
+++ src/context/productManagment.tsx
@@ -1,5 +1,5 @@
-// src/context/cartSlice.ts
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { products } from "@/utils/GetAPI";
 
 export interface CartItem {
   id: string;
@@ -21,9 +21,20 @@ if (typeof window !== "undefined") {
     const storedCart = localStorage.getItem("cart");
     if (storedCart) {
       const parsed = JSON.parse(storedCart);
+
+      const mergedItems = (Array.isArray(parsed.items) ? parsed.items : []).map((item: any) => {
+        const fullProduct = products.find((p) => p.id === item.id);
+        return {
+          ...item,
+          name: fullProduct?.title ?? item.name ?? "Unknown",
+          price: fullProduct?.price ?? item.price ?? 0,
+          img : fullProduct?.img ?? item.img ?? ""
+        };
+      });
+
       initialState = {
-        items: Array.isArray(parsed.items) ? parsed.items : [],
-        totalAmount: typeof parsed.totalAmount === "number" ? parsed.totalAmount : 0,
+        items: mergedItems,
+        totalAmount: mergedItems.reduce((sum: any, i: any) => sum + i.price * i.quantity, 0),
       };
     }
   } catch {
@@ -42,28 +53,40 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action: PayloadAction<CartItem>) => {
-      const existingItem = state.items.find(i => i.id === action.payload.id);
+      const existingItem = state.items.find((i) => i.id === action.payload.id);
+
+      // const fullProduct = products.find((p) => p.id === action.payload.id);
+      // const price = fullProduct?.price ?? action.payload.price ?? 0;
+      // const name = fullProduct?.title ?? action.payload.name ?? "Unknown";
+      // const img = fullProduct?.img ?? action.payload.img ?? "";
+
       if (existingItem) {
         existingItem.quantity += 1;
       } else {
+
         state.items.push({ ...action.payload, quantity: 1 });
+
       }
+
       state.totalAmount = state.items.reduce(
         (sum, item) => sum + item.price * item.quantity,
         0
       );
+
       saveToLocalStorage(state);
     },
 
     decreaseQuantity: (state, action: PayloadAction<string>) => {
       if (!state.items) state.items = [];
-      const item = state.items.find(i => i.id === action.payload);
+      const item = state.items.find((i) => i.id === action.payload);
       if (!item) return;
+
       if (item.quantity > 1) {
         item.quantity -= 1;
       } else {
-        state.items = state.items.filter(i => i.id !== action.payload);
+        state.items = state.items.filter((i) => i.id !== action.payload);
       }
+
       state.totalAmount = state.items.reduce(
         (sum, i) => sum + i.price * i.quantity,
         0
