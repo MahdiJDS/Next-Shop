09a40ed861e8d42deff0a1a35881e23323e34560
Mahdi Jahed <mahdijahed56@gmail.com>
2025-11-11 17:11:38 +0330

    feat: cart Page and use allItems components

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AllItems from './components/allItems'
+import Container from '@/components/Conteiner'
+
+export default function page() {
+  return (
+    <Container>
+        <AllItems />
+    </Container>
+  )
+}
