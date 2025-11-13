3670e9845346777614d81e25da5a8575594ce73e
Mahdi Jahed <mahdijahed56@gmail.com>
2025-11-13 17:39:52 +0330

    feat: Add buyList from Products types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,14 @@
+import { CartItem } from "./context/productManagment"
+
 export interface jsonType {
     id: string,
     title: string,
     img: string,
     description: string,
     price: number
+}
+
+export interface buyType{
+    items:CartItem[],
+    totalPrice: number
 }
